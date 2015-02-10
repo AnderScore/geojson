7ffe125cc28ecb4e0624c51a1818aa2b0a51ba0c
Anders <theb00gieman@hotmail.com>
2015-02-10 21:34:01 +0100

    load geojson with ajax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-var geojsonLayer = new L.GeoJSON.AJAX("https://raw.githubusercontent.com/AnderScore/geojson/master/55.geojson");
+var geojsonLayer = new L.GeoJSON.AJAX("https://rawgit.com/AnderScore/geojson/master/55.geojson");
 
-var map = L.map('map').fitBounds(geojsonLayer.getBounds());
+var map = L.map('map').setView([59.3312609, 18.072064], 12);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 	maxZoom: 18,
